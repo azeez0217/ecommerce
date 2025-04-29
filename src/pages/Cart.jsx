4c3a064dcf32872dd397dfe18b0c09787a11446c
Azeez <skazeez0217@gmail.com>
2025-04-29 22:39:05 +0530

    Small chnages made

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cart =()=>{
     return(
         <div>
-            <div>Hellow I am the Cart Page Welcome</div>
+            <div>Hellow I am the Cart Page </div>
         </div>
     )
 }
